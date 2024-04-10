31e312096247a64112e2a592e20ecea8603ea7c0
JonasSeifried <69268825+JonasSeifried@users.noreply.github.com>
2024-04-10 13:18:23 +0200

    new input methode

--- client/src/views/FreeForAll/FreeForAll.vue
+++ client/src/views/FreeForAll/FreeForAll.vue
@@ -2,22 +2,11 @@
 import { onBeforeMount, ref, type VNodeRef } from 'vue'
 import { useRoute } from 'vue-router'
 import { useRoomStore } from '@/stores/room'
-import FreeForAllPreGame from './FreeForAllPreGame.vue'
 import FreeForAllGame from './FreeForAllGame.vue'
 
-const writableDiv = ref<HTMLDivElement | null>(null)
 const route = useRoute()
 const roomStore = useRoomStore()
 var roomId = route.params.roomId as string
-
-
-
-function onDivClick() {
-    console.log('clicked', writableDiv.value)
-    if (!writableDiv.value) return
-    writableDiv.value.focus()
-}
-
 
 onBeforeMount(() => {
     if (roomStore.isInRoom()) return
@@ -32,19 +21,8 @@ onBeforeMount(() => {
 <template>
     <div class="flex flex-col items-center mx-10 w-full h-full">
         <h1 class="mb-5 text-2xl text-white">Typing Domain</h1>
-        <div :onClick="onDivClick" class="p-8 bg-stone-700 w-3/5 rounded flex flex-row focus-within:outline text-xl">
-            <div class="flex w-48">
-
-                <div class="bg-stone-600 p-4 flex-grow"></div>
-                <div ref="writableDiv" class="w-max text-nowrap max-w-48 whitespace-pre overflow-hidden focus:outline-none" contenteditable="true"></div>
-            </div>
-            <div class="bg-stone-800 w-48 p-4"></div>
-            <!-- <wbr> -->
-        </div>
-        <!-- <FreeForAllPreGame v-if="roomStore.isWaiting" />
-        <FreeForAllGame v-if="roomStore.isPlaying" /> -->
+        <!-- <FreeForAllPreGame v-if="roomStore.isWaiting" /> -->
+        <FreeForAllGame v-if="roomStore.isPlaying || roomStore.textOfRoom !== ''" />
         <!-- <FreeForAllPostGame v-if="roomStore.isFinished" />   -->
     </div>
 </template>
-
-
--- client/src/views/FreeForAll/FreeForAllGame.vue
+++ client/src/views/FreeForAll/FreeForAllGame.vue
@@ -1,50 +1,104 @@
 <script setup lang="ts">
 import FreeForAllProgressBars from './FreeForAllProgressBars.vue'
-import { socket } from '@/socket'
 import { useRoomStore } from '@/stores/room'
-import { ref } from 'vue'
-import { useRoute } from 'vue-router'
+import { computed, onMounted, ref } from 'vue'
 
-const hiddenInputField = ref<HTMLInputElement | null>(null)
-const typedText = ref<string>('')
-const route = useRoute()
 const roomStore = useRoomStore()
-var roomId = route.params.roomId as string
-var lastInputEmited: string | null = null
 
-function onInput() {
-    if (lastInputEmited === typedText.value) return
-    socket.emit('textFieldInput', typedText.value)
-    lastInputEmited = typedText.value
+const writableDiv = ref<HTMLDivElement | null>(null)
+const typedText = ref<string>('')
+const editableText = ref<string>('')
+const textToWrite = computed<string>(() => remainingText.value.split(' ', 2).join(' '))
+const remainingText = ref<string>('')
+
+function onInput(payload: Event) {
+    // if (lastInputEmited === typedText.value) return
+    // socket.emit('textFieldInput', typedText.value)
+    // lastInputEmited = typedText.value
+    if (!writableDiv.value) return
+    editableText.value = writableDiv.value.innerText
+    return
+}
+
+function onKeyDown(event: KeyboardEvent) {
+    if (event.key === 'Enter') {
+        event.preventDefault()
+        return
+    }
+    if (event.key !== ' ') return
+    if (!writableDiv.value) return
+    const trimmedText = writableDiv.value.innerText.trim()
+    if (!trimmedText.includes(' ')) return
+    const indexOfSpace = trimmedText.indexOf(' ')
+    const firstWord = trimmedText.substring(0, indexOfSpace + 1)
+    const secondWord = trimmedText.substring(indexOfSpace + 1)
+    typedText.value += firstWord
+    writableDiv.value.innerText = secondWord
+    remainingText.value = remainingText.value.slice(firstWord.length)
+
+    setCursorToEnd()
+}
+
+function setCursorToEnd() {
+    if (!writableDiv.value) return
+    const range = document.createRange()
+    const selection = window.getSelection()
+    range.selectNodeContents(writableDiv.value)
+    range.collapse(false)
+    selection?.removeAllRanges()
+    selection?.addRange(range)
+    writableDiv.value.focus()
+}
+
+function onDivClick() {
+    setCursorToEnd()
 }
 
 function letterClass(letter: string, index: number): string {
-    if (typedText.value.length <= index) return 'white_letter'
-    return letter === typedText.value[index] ? 'green_letter' : 'red_letter'
+    return letter === textToWrite.value[index] ? 'green_letter' : 'red_letter'
 }
+
+onMounted(() => {
+    //remainingText.value = roomStore.textOfRoom
+    remainingText.value =
+        'The quick brown fox jumps over the lazy dog The quick brown fox jumps over the lazy dog The quick brown fox jumps over the lazy dog The quick brown fox jumps over the lazy dog The quick brown fox jumps over the lazy dog The quick brown fox jumps over the lazy dog'
+    setCursorToEnd()
+})
 </script>
 
 <template>
     <FreeForAllProgressBars />
     <span class="relative right-0 ml-20">{{ roomStore.gameTimer }}</span>
-    <div class="flex flex-wrap w-1/2 gap-y-1">
-        <span
-            v-for="(item, index) in roomStore.textOfRoom"
-            :key="index"
-            class="letter text-xl rounded-[0.25rem] border-[1px] border-white border-opacity-5 whitespace-pre"
-            :class="letterClass(item, index)"
-        >
-            {{ item }}
-        </span>
+    <div
+        :onClick="onDivClick"
+        class="p-8 bg-stone-700 rounded flex flex-row justify-center focus-within:outline text-2xl letter whitespace-pre select-none"
+    >
+        <div class="flex justify-end w-96 bg-stone-600 text-nowrap py-4 gap-1 text-right">
+            <div class="min-w-0 text-opacity-50">
+                {{ typedText }}
+            </div>
+        </div>
+        <div class="bg-stone-800 w-96 py-4">
+            <div
+                ref="writableDiv"
+                class="w-min min-w-16 overflow-hidden focus:outline-none absolute text-transparent caret-white z-20"
+                contenteditable="true"
+                @keydown="onKeyDown"
+                @input="onInput"
+            ></div>
+            <div class="w-min min-w-16 overflow-hidden focus:outline-none z-10 absolute">
+                <span
+                    v-for="(item, index) in editableText"
+                    :key="index"
+                    class="bg-stone-800"
+                    :class="letterClass(item, index)"
+                >
+                    {{ item }}
+                </span>
+            </div>
+            <div class="truncate">{{ remainingText }}</div>
+        </div>
     </div>
-    <input
-        ref="hiddenInputField"
-        v-model="typedText"
-        @input="onInput"
-        :maxlength="roomStore.textOfRoom.length"
-        class="hidden-input-field"
-        autofocus
-    />
 </template>
 
 <style scoped>
@@ -53,11 +107,10 @@ function letterClass(letter: string, index: number): string {
 }
 .green_letter {
     color: rgb(30, 153, 30);
-    background: rgba(30, 153, 30, 0.15);
 }
 .red_letter {
     color: rgb(170, 26, 26);
-    background: rgba(170, 26, 26, 0.15);
+    text-decoration: line-through;
 }
 .hidden-input-field {
     position: absolute;
