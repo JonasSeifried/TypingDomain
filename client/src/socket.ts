4d15cb28d61e9e25c50dfb71a55e9fb7500563c8
JonasSeifried <69268825+JonasSeifried@users.noreply.github.com>
2024-04-20 00:08:49 +0200

    fix socket connection

--- client/src/socket.ts
+++ client/src/socket.ts
@@ -3,12 +3,13 @@ import type { ClientToServerEvents, ServerToCLientEvents } from 'shared'
 import { useSocketStore } from './stores/socket'
 import { useRoomStore } from './stores/room'
 
-export const socket: Socket<ServerToCLientEvents, ClientToServerEvents> = io(
-    'http://localhost:3000',
-    {
-        autoConnect: false
-    }
-)
+const URL =
+    process.env.NODE_ENV === 'production'
+        ? window.location.origin + '/backend'
+        : 'http://localhost:3000'
+export const socket: Socket<ServerToCLientEvents, ClientToServerEvents> = io(URL, {
+    autoConnect: false
+})
 
 export function bindEvents() {
     useSocketStore().bindEvents()
