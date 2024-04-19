403093f243fe90da14921c542518dc65252404e1
JonasSeifried <69268825+JonasSeifried@users.noreply.github.com>
2024-04-20 01:02:28 +0200

    undo socket io confg

--- client/src/socket.ts
+++ client/src/socket.ts
@@ -3,10 +3,7 @@ import type { ClientToServerEvents, ServerToCLientEvents } from 'shared'
 import { useSocketStore } from './stores/socket'
 import { useRoomStore } from './stores/room'
 
-const URL =
-    process.env.NODE_ENV === 'production'
-        ? window.location.origin + '/backend'
-        : 'http://localhost:3000'
+const URL = process.env.NODE_ENV === 'production' ? undefined : 'http://localhost:3000'
 export const socket: Socket<ServerToCLientEvents, ClientToServerEvents> = io(URL, {
     autoConnect: false
 })
