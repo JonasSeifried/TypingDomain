5aa87fe289349c4c8c3bf5d99bd6c879420936c7
JonasSeifried <69268825+JonasSeifried@users.noreply.github.com>
2024-04-20 21:23:34 +0200

    Set Text to Lorem Ipsum

--- server/src/server.ts
+++ server/src/server.ts
@@ -178,8 +178,7 @@ function startRoomCountDown(roomId: string) {
             clearInterval(interval)
             rooms.setRoomText(
                 roomId,
-                'This is a short text example to test the game'
-                //"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+                "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
             )
             rooms.startGame(roomId)
             startRoomPlayTimer(roomId)
