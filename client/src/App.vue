d14bec1c2eaaf4e29a01fc885410f44034b4ac78
JonasSeifried <69268825+JonasSeifried@users.noreply.github.com>
2024-04-22 11:22:29 +0200

    Add ToggleColorScheme

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,10 +1,11 @@
 <script setup lang="ts">
-import { onBeforeMount } from 'vue'
+import { computed, onBeforeMount, ref } from 'vue'
 import { socket, bindEvents } from './socket'
 import { useAuthStore } from './stores/auth'
 import { useSocketStore } from './stores/socket'
 import { useRouter } from 'vue-router'
 import { HomeRoute } from './router'
+import ToggleColorScheme from './components/ToggleColorScheme.vue'
 
 const authStore = useAuthStore()
 const connectionStore = useSocketStore()
@@ -18,22 +19,21 @@ onBeforeMount(() => {
 </script>
 
 <template>
-    <header class="grid w-full grid-cols-3 justify-evenly">
+    <header class="grid justify-between w-full grid-cols-3 px-4 text-xl sm:px-8">
         <button
             v-if="authStore.isSignedIn"
-            class="p-4 text-xl hover:text-white w-fit hover:scale-110"
+            class="w-fit hover:scale-110"
             @click="authStore.signOut"
         >
             Sign Out
         </button>
-        <a
-            href="#"
-            class="col-start-2 m-4 text-2xl text-center text-white"
-            @click="router.push(HomeRoute)"
-        >
+        <a href="#" class="col-start-2 m-4 text-2xl text-center" @click="router.push(HomeRoute)">
             Typing Domain
         </a>
-        <span class="m-4 text-xl text-right">{{ authStore.username }}</span>
+        <div class="flex items-center justify-end h-full gap-4">
+            <ToggleColorScheme />
+            <span class="text-right">{{ authStore.username }}</span>
+        </div>
     </header>
     <main class="flex justify-center w-full h-full">
         <div class="container flex flex-col items-center h-full">
